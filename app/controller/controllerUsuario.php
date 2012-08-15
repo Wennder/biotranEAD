f72e2c10a2ed6feff37a8aff20909abaaee643ea
cead-p057007 <cead-p057007@172.16.194.220>
2012-08-15 13:38:10 -0300

    VERSAO 0.1 - preparação para mesclar projeto

--- app/controller/controllerUsuario.php
+++ app/controller/controllerUsuario.php
@@ -13,16 +13,27 @@
 class controllerUsuario {
     
     private $usuario;
+    private $end_residencial;
+    private $end_comercial;
 
     public function novoUsuario(Usuario $user = null) {
         if (!empty($_POST)) {
             $this->usuario = new Usuario();
+            $this->end_comercial = new Endereco();
+            $this->end_residencial = new Endereco();
             foreach ($_POST as $k => $v) {
                 $setAtributo = 'set'.ucfirst($k);
                 if(method_exists($this->usuario, $setAtributo)){
                     $this->usuario->$setAtributo($v);                                    
+                }else{
+                    if(method_exists($this->end_comercial, $method_name)){
+                        
+                    }
                 }
+                
             }
+            $dao = new UsuarioDAO();
+            $dao->insert($user, $end1, $end2);
             
         }else{
             if($user != null){
