94d017d6d14cc74fd384d92cdae9ae5b1cbbe0b5
cead-p057007 <cead-p057007@172.16.194.220>
2012-08-16 09:47:48 -0300

    VERSAO 0.1 - projeto mesclado

--- app/controller/ControllerEad.php
+++ app/controller/ControllerEad.php
@@ -21,8 +21,9 @@ class ControllerEad extends Biotran_Mvc_Controller{
     }
     
     public function actionCadastrar_usuario() {        
-        $ctrl = new controllerUsuario();
+        $ctrl = new controllerUsuario();    
         $ctrl->novoUsuario();
+        Biotran_Mvc::pegarInstancia()->mudarAcao('gerenciar_usuarios');
         $this->renderizar();
     }
     
