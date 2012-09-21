a8ab0e60812ddf9de467bb5120b424390e1a8fdf
Vinicius <Vinicius@Vinicius-PC.mshome.net>
2012-09-21 10:49:06 -0300

    Padronização das leftcolumns

--- app/view/ead/structure/leftcolumn_aluno_home.php
+++ app/view/ead/structure/leftcolumn_aluno_home.php
@@ -1,5 +1,6 @@
 <script src="js/jquery-ui-1.8.23.custom.min.js" type="text/javascript"></script>
 
+
 <div id="page-leftcolumn" class="leftcolumn page-leftcolumn">
     <h3 class="navbar_item homeIcon">
         <a href="index.php?c=ead"> Home </a>
