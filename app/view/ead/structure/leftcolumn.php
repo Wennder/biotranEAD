a4c5416e3a4117be899ef3b904fa94153ab93128
Vinicius <Vinicius@Vinicius-PC.mshome.net>
2012-10-02 09:49:22 -0300

    leftcolumns com link

--- app/view/ead/structure/leftcolumn.php
+++ app/view/ead/structure/leftcolumn.php
@@ -1,5 +1,7 @@
 <script src="js/jquery-ui-1.8.23.custom.min.js" type="text/javascript"></script>
 <script src="js/accordion.js" type="text/javascript"></script>
+
+
 <?php
 $usuario = $_SESSION["usuarioLogado"]->getId_papel();
 if (isset($_GET['id'])) {
